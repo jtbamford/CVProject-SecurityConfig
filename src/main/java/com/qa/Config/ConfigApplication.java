cc3a0c733d9beb71ff5476e682b511ff59cb65c4
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-12-13 16:05:09 +0000

    commit

--- src/main/java/com/qa/Config/ConfigApplication.java
+++ src/main/java/com/qa/Config/ConfigApplication.java
@@ -2,10 +2,8 @@ package com.qa.Config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
